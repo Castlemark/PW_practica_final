3db91f4eb37e378c70f7cedff9add98d8e67588a
Marc <marc.castells.guell@gmail.com>
2017-12-12 22:56:45 +0100

    exercici 9 acabat

--- scripts/main.js
+++ scripts/main.js
@@ -1,11 +1,22 @@
 var videos = [];
-var People;
+var response;
+var popular;
 
 //ASYNC
 function reqListener () {
-  //var People = eval(this.responseText);
-  People = JSON.parse(this.responseText);
-  console.log(People);
+  response = JSON.parse(this.responseText);
+
+  for (var i = 0; i < response.items.length; i++) {
+    document.getElementById("searchRow").appendChild(youtubeNode(i, 0));
+  }
+}
+
+function loadPopular () {
+  popular = JSON.parse(this.responseText);
+
+  for (var i = 0; i < popular.items.length; i++) {
+    document.getElementById("popularesRow").appendChild(youtubeNode(i, 1));
+  }
 }
 
 function searchVideos() {
@@ -20,32 +31,29 @@ function searchVideos() {
     request: function (url){
       var xhr = new XMLHttpRequest();
       xhr.addEventListener("load", reqListener);
-      xhr.open("GET", "https://www.googleapis.com/youtube/v3/search?part=snippet&videoEmbeddable=true&order=viewCount&q=" + text + "&maxResults=10&type=video&videoDefinition=high&key=AIzaSyA4fK-XwuzdoWRTs7l0L9UdVyBz4UHg8ik", true);
+      xhr.open("GET", url, true);
       xhr.send();
     }
   };
 
-  ajaxASYNC.request("https://swapi.co/api/people/1/");
+  ajaxASYNC.request("https://www.googleapis.com/youtube/v3/search?part=snippet&videoEmbeddable=true&order=viewCount&q=" + text + "&maxResults=10&type=video&videoDefinition=high&key=AIzaSyA4fK-XwuzdoWRTs7l0L9UdVyBz4UHg8ik");
 
-  console.log(People)
-
-  var total = 0;
-  document.getElementById("searchRow").innerHTML = "";
-  videos.forEach(function(element, index) {
-    // Case-insensitive search
-    if (element.name.toLowerCase().indexOf(text) != -1) {
-      total++;
-      document.getElementById("searchRow").appendChild(nodeFromVideo(element));
-    }
-  });
   document.getElementById("resultados").style.display = "block";
-  if (total == 0) {
-    document.getElementById("searchRow").appendChild(noResultsFoundNode());
+}
+
+function youtubeNode (i, type){
+  var item;
+  var tipo;
+
+  if (type == 0) {
+    item = response.items[i];
+    tipo = "resultado ";
   }
-}
+  else {
+    item = popular.items[i];
+    tipo = "popular ";
+  }
 
-
-function youtubeNode (){
   var divNode = document.createElement("div");
   divNode.setAttribute("class", "col-sm-12 col-md-6 col-lg-4 col-xl-4");
 
@@ -53,38 +61,17 @@ function youtubeNode (){
   divNode.appendChild(figureNode);
 
   var imgNode = document.createElement("img");
-  imgNode.setAttribute("src", "https://i.ytimg.com/vi/" + video.id + "/hqdefault.jpg");
-  imgNode.setAttribute("alt", "Vista previa del vídeo llamado " + video.name);
+  imgNode.setAttribute("src", item.snippet.thumbnails.high.url);
+  imgNode.setAttribute("alt", "Vista previa del vídeo llamado " + item.snippet.title);
   figureNode.appendChild(imgNode);
 
   var figcaptionNode = document.createElement("figcaption");
   var h6Node = document.createElement("h6");
-  var textnode = document.createTextNode(video.name);
+  var textnode = document.createTextNode(item.snippet.title);
   h6Node.appendChild(textnode);
   figcaptionNode.appendChild(h6Node);
   figureNode.appendChild(figcaptionNode);
-
-  return divNode;
-}
-
-function nodeFromVideo(video) {
-  var divNode = document.createElement("div");
-  divNode.setAttribute("class", "col-sm-12 col-md-6 col-lg-4 col-xl-4");
-
-  var figureNode = document.createElement("figure");
-  divNode.appendChild(figureNode);
-
-  var imgNode = document.createElement("img");
-  imgNode.setAttribute("src", "https://i.ytimg.com/vi/" + video.id + "/hqdefault.jpg");
-  imgNode.setAttribute("alt", "Vista previa del vídeo llamado " + video.name);
-  figureNode.appendChild(imgNode);
-
-  var figcaptionNode = document.createElement("figcaption");
-  var h6Node = document.createElement("h6");
-  var textnode = document.createTextNode(video.name);
-  h6Node.appendChild(textnode);
-  figcaptionNode.appendChild(h6Node);
-  figureNode.appendChild(figcaptionNode);
+  console.log(tipo + (i+1) + " : " + item.snippet.title);
 
   return divNode;
 }
@@ -98,44 +85,16 @@ function noResultsFoundNode() {
 
 window.onload = function() {
 
-  var video1 = {id: "Xhbyj8pqUao", name: "The Velvet Underground, Nico - Sunday Morning", thumbnailServer: "img.youtube.com"};
-  var video2 = {id: "nkumhBVPGdg", name: "The Velvet Underground - Sweet Jane", thumbnailServer: "img.youtube.com"};
-  var video3 = {id: "H5je_eK0V1w", name: "Velvet Underground - I'm Sticking With You", thumbnailServer: "img.youtube.com"};
-  var video4 = {id: "m3lF2qEA2cw", name: "Creep - Vintage Postmodern Jukebox Radiohead Cover ft. Haley Reinhart", thumbnailServer: "i.ytimg.com"};
-
-  var video5 = {id: "arHwjrfIT-M", name: "[Video C] 바닷물에 잠긴 부산 해운대 마린시티", thumbnailServer: "i.ytimg.com"};
-  var video6 = {id: "TAkpv5AD0yk", name: "リンカーン芸人大運動会2016 2016.10.05", thumbnailServer: "i.ytimg.com"};
-  var video7 = {id: "2xhPzlwrNvw", name: "【ゆっくり茶番】殺人パン！？ゆっくり達の文化祭 #1！【たくっち】", thumbnailServer: "i.ytimg.com"};
-  var video8 = {id: "IBoOaAVpW2U", name: "【マインクラフト】ガチャガチャ作ってみた！当たるかな!?【ヒカキンのマイクラ実況 Part253】【ヒカクラ】", thumbnailServer: "i.ytimg.com"};
-
-  var video9 = {id: "kwJYhK9ZeX4", name: "EL PSICÓLOGO LOCO", thumbnailServer: "i.ytimg.com"};
-  var video10 = {id: "ZRmnVxSzJX8", name: "EL AGENTE 87 (Broma telefónica)", thumbnailServer: "i.ytimg.com"};
-  var video11 = {id: "kSHReKGK_VY", name: "¡Gran rajada de 'Pichu' Cuéllar contra un periodista! ¡Sin palabras!", thumbnailServer: "i.ytimg.com"};
-  var video12 = {id: "qlhzKJZ-SbU", name: "AQUÍ ESTA MI NUEVO CANAL!! KOKO DC", thumbnailServer: "i.ytimg.com"};
-
-  videos.push(video1);
-  videos.push(video2);
-  videos.push(video3);
-  videos.push(video4);
-
-  videos.push(video5);
-  videos.push(video6);
-  videos.push(video7);
-  videos.push(video8);
-
-  videos.push(video9);
-  videos.push(video10);
-  videos.push(video11);
-  videos.push(video12);
-
-  videos.forEach(function(element, index) {
-    if (index >= 0 && index <= 7) {
-      document.getElementById("vistosRow").appendChild(nodeFromVideo(element));
-      return;
+  var ajaxASYNC = {
+    request: function (url){
+      var xhr = new XMLHttpRequest();
+      xhr.addEventListener("load", loadPopular);
+      xhr.open("GET", url, true);
+      xhr.send();
     }
-    if (index >= 8 && index <= 11) {
-      document.getElementById("popularesRow").appendChild(nodeFromVideo(element));
-      return;
-    }
-  });
+  };
+
+  ajaxASYNC.request("https://www.googleapis.com/youtube/v3/videos?chart=MostPopular&regionCode=es&maxResults=10&videoCategoryId=0&key=AIzaSyA4fK-XwuzdoWRTs7l0L9UdVyBz4UHg8ik&part=snippet,player");
+
+  document.getElementById("resultados").style.display = "block";
 };
